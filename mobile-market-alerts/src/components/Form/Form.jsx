f265dba7299588254ef6916181f1d0d13d121ef8
Jessica McCollum <info.jessicamccollum@gmail.com>
2021-03-26 14:26:01 -0500

    More styling for the form

--- mobile-market-alerts/src/components/Form/Form.jsx
+++ mobile-market-alerts/src/components/Form/Form.jsx
@@ -6,25 +6,35 @@ class Form extends Component {
   render() {
     return (
         <form class="form">
-          <h3>Sign Up Here</h3>
-          <br></br>
-          <label for="phone">Cell Phone Number</label>
-          <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"></input>
-          <br></br>
-          <label for="location">Location</label>
-          <input type="text" id="location" name="location"></input>
-          <br/>
-          <h4>Check All That Apply:</h4>
-          <input type="checkbox" id="farmersmarket" name="farmersmarket" value="Farmers Market"></input>
-          <label for="farmersmarket">Farmers Market</label>
-          <br/>
-          <input type="checkbox" id="freemeals" name="freemeals" value="Free Meals"></input>
-          <label for="freemeals">Free Meals</label>
-          <br/>
-          <input type="checkbox" id="foodshelves" name="foodshelves" value="Food Shelves"></input>
-          <label for="foodshelves">Food Shelves</label>
-          <br/>
-          <input type="submit" name="submit" value="submit"></input>
+          <div class="container">
+            <h3>Sign Up Here</h3>
+          </div>
+          <div class="container text-input">
+            <label for="phone">Cell Phone Number</label>
+            <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"></input>
+          </div>
+          <div class="container text-input">
+            <label for="location">Location</label>
+            <input type="text" id="location" name="location"></input>
+          </div>
+          <div class="container check-label">
+            <label>Check All That Apply:</label>
+          </div>
+          <div class="container checkbox-box">
+            <input type="checkbox" class="checkbox-input" name="farmersmarket" value="Farmers Market"></input>
+            <label for="farmersmarket">Farmers Market</label>
+          </div>
+          <div class="container">
+            <input type="checkbox" class="checkbox-input" name="freemeals" value="Free Meals"></input>
+            <label for="freemeals">Free Meals</label>
+          </div>
+          <div class="container">
+            <input type="checkbox" class="checkbox-input" name="foodshelves" value="Food Shelves"></input>
+            <label for="foodshelves">Food Shelves</label>
+          </div>
+            <div class="container">
+            <input type="submit" name="submit" value="submit"></input>
+          </div>
         </form>
         
     )
