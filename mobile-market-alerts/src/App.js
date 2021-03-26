140d372af445872514c594b19878bc8f595967c6
Kaylin Bittner <kaylinbittner@gmail.com>
2021-03-26 15:18:55 -0400

    basic footer and banner styling

--- mobile-market-alerts/src/App.js
+++ mobile-market-alerts/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Banner from './components/Banner/Banner.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
-function App() {
-  return (
-    <Banner />
-  );
+class App extends Component {
+  render () {
+    return (
+      // React.Fragment exists to wrap JSX elements in an enclosing tag
+      <React.Fragment>
+        <Banner />
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
+
 
 export default App;
