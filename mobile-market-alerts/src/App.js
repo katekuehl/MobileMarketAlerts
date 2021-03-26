932de4746195e3c84ab816d8818984ea86288653
Jessica McCollum <info.jessicamccollum@gmail.com>
2021-03-26 11:57:38 -0500

    Form setup: Components/Form folder, Form.CSS, and Form.jsx. Added Form to App.js

--- mobile-market-alerts/src/App.js
+++ mobile-market-alerts/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 import Banner from './components/Banner/Banner.jsx';
+import Form from './components/Form/Form.jsx';
 
 function App() {
   return (
-    <Banner />
+    <div>
+      <Banner />
+      <Form />
+    </div>
+
+
+    
+
   );
 }
 
---
+++ mobile-market-alerts/src/components/Form/Form.jsx
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+import './Form.css';
+
+
+class Form extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Sign Up Here</h3>
+      </div>
+    )
+  }
+}
+
+export default Form;
