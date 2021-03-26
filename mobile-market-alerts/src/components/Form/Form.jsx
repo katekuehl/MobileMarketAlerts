c0073f2919b69533b4efab0593bdf208e4d69615
Jessica McCollum <info.jessicamccollum@gmail.com>
2021-03-26 13:07:15 -0500

    Form outline

--- mobile-market-alerts/src/components/Form/Form.jsx
+++ mobile-market-alerts/src/components/Form/Form.jsx
@@ -5,9 +5,28 @@ import './Form.css';
 class Form extends Component {
   render() {
     return (
-      <div>
-        <h3>Sign Up Here</h3>
-      </div>
+        <form>
+          <h3>Sign Up Here</h3>
+          <br></br>
+          <label for="phone">Cell Phone Number</label>
+          <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"></input>
+          <br></br>
+          <label for="location">Location</label>
+          <input type="text" id="location" name="location"></input>
+          <br/>
+          <h4>Check All That Apply:</h4>
+          <input type="checkbox" id="farmersmarket" name="farmersmarket" value="Farmers Market"></input>
+          <label for="farmersmarket">Farmers Market</label>
+          <br/>
+          <input type="checkbox" id="freemeals" name="freemeals" value="Free Meals"></input>
+          <label for="freemeals">Free Meals</label>
+          <br/>
+          <input type="checkbox" id="foodshelves" name="foodshelves" value="Food Shelves"></input>
+          <label for="foodshelves">Food Shelves</label>
+          <br/>
+          <input type="submit" name="submit" value="submit"></input>
+        </form>
+        
     )
   }
 }
