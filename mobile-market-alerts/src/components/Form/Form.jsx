159c617cdd52ad9e43345cb481a9d9ec631d710a
Jessica McCollum <info.jessicamccollum@gmail.com>
2021-03-26 16:17:45 -0500

    form styling

--- mobile-market-alerts/src/components/Form/Form.jsx
+++ mobile-market-alerts/src/components/Form/Form.jsx
@@ -9,30 +9,30 @@ class Form extends Component {
           <div class="container">
             <h3>Sign Up Here</h3>
           </div>
-          <div class="container text-input">
+          <div class="container text-input phone">
             <label for="phone">Cell Phone Number</label>
             <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"></input>
           </div>
-          <div class="container text-input">
+          <div class="container text-input location">
             <label for="location">Location</label>
             <input type="text" id="location" name="location"></input>
           </div>
-          <div class="container check-label">
+          <div class="container check-label check-location">
             <label>Check All That Apply:</label>
           </div>
           <div class="container checkbox-box">
             <input type="checkbox" class="checkbox-input" name="farmersmarket" value="Farmers Market"></input>
             <label for="farmersmarket">Farmers Market</label>
           </div>
-          <div class="container">
+          <div class="container checkbox-box">
             <input type="checkbox" class="checkbox-input" name="freemeals" value="Free Meals"></input>
             <label for="freemeals">Free Meals</label>
           </div>
-          <div class="container">
+          <div class="container checkbox-box">
             <input type="checkbox" class="checkbox-input" name="foodshelves" value="Food Shelves"></input>
             <label for="foodshelves">Food Shelves</label>
           </div>
-            <div class="container">
+            <div class="container submit-button">
             <input type="submit" name="submit" value="submit"></input>
           </div>
         </form>
