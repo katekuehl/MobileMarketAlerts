f6f7c8d368c682423ff74ed4d119fa90e3f3451b
Jessica McCollum <info.jessicamccollum@gmail.com>
2021-03-26 13:22:39 -0500

    How This Works. Created folder and files. Updated App.js

--- mobile-market-alerts/src/App.js
+++ mobile-market-alerts/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Banner from './components/Banner/Banner.jsx';
 import Form from './components/Form/Form.jsx';
+import HowThisWorks from './components/HowThisWorks/HowThisWorks';
 
 function App() {
   return (
     <div>
       <Banner />
+      <HowThisWorks />
       <Form />
     </div>
 
---
+++ mobile-market-alerts/src/components/HowThisWorks/HowThisWorks.jsx
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+import './HowThisWorks.css';
+
+
+class HowThisWorks extends Component {
+  render() {
+    return (
+        <div>
+          <h3>How This Works</h3>
+        </div>
+    )
+  }
+}
+
+export default HowThisWorks;
